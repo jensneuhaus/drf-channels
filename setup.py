8628594dab388579cc283d4270947fb2cfbefdde
Rieljun Liguid <me@iamriel.com>
2018-05-30 23:14:22 +0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
 from drf_channels import __version__
 
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='drf_channels',
     version=__version__,
-    url='https://github.com/linuxlewis/channels-api',
+    url='https://github.com/iamriel/drf-channels',
     author='Rieljun Liguid',
     author_email='me@iamriel.com',
     description='Simple Resource binding and Consumer Mixin for Django Rest Framework and Channels 2',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
